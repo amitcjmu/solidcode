88512b1b0fcbe0039938e3e43e310d3c4e021062
Amit <achoudhary7@sapient.com>
2016-11-29 11:50:57 +0530

    Added Constant file

---
+++ clean/src/main/java/com/prep/rockpaper/config/RockPaperConstants.java
@@ -0,0 +1,13 @@
+package com.prep.rockpaper.config;
+
+/*
+ * For defining any constants with this application.
+ * This constant file should be used.
+ */
+public final class RockPaperConstants {
+
+	public static final String ROCK = "ROCK";
+	public static final String PAPER = "PAPER";
+	public static final String SCISSOR = "SCISSOR";
+	public static final String TIE = "TIE";
+}

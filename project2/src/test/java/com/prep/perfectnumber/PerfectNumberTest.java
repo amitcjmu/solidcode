f768fb9af09901ebb910b29dd2b3488c17f771a8
Amit <achoudhary7@sapient.com>
2016-11-30 10:04:54 +0530

    RED: Added PerfectNumberTest class

---
+++ project2/src/test/java/com/prep/perfectnumber/PerfectNumberTest.java
@@ -0,0 +1,16 @@
+package com.prep.perfectnumber;
+
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class PerfectNumberTest {
+
+	PerfectNumber perfectNumber;
+	
+	@Test
+	public void testPerfectNumber() {
+		Integer number = 6;
+		perfectNumber = new perfectNumber(number );
+	}
+}

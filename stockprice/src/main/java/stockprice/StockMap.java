952fb6e1e65bfacbfbabdded206b3de16ebc7bfb
Amit <achoudhary7@sapient.com>
2016-12-02 09:42:39 +0530

    Added Enum

---
+++ stockprice/src/main/java/stockprice/StockMap.java
@@ -0,0 +1,29 @@
+package stockprice;
+public enum StockMap {
+
+ A {
+    @Override
+    public String toString() {
+      return "50";
+    }
+  },
+  B {
+    @Override
+    public String toString() {
+      return "30";
+    }
+  },
+  C {
+    @Override
+    public String toString() {
+      return "20";
+    }
+  },
+  D {
+	    @Override
+	    public String toString() {
+	      return "15";
+	    }
+	  }
+
+}

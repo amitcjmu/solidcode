cccaa693f5416e2fedbbdb8fe362fb05b380f348
Amit <achoudhary7@sapient.com>
2016-12-03 12:39:28 +0530

    Green: added converter method to convert 1-19 numbers to words

--- number-to-words/src/main/java/com/prep/rockpaper/NumberToWordsConvertor.java
+++ number-to-words/src/main/java/com/prep/rockpaper/NumberToWordsConvertor.java
@@ -1,13 +1,29 @@
 package com.prep.rockpaper;
 
-public class NumberToWordsConvertor {
+import java.util.ArrayList;
+import java.util.List;
 
-	public String getWordsForNumber(Integer i) {
-		if (i.equals(0)) {
-			return "Zero";
-		} else {
-			return "One";
-		}
+public class NumberToWordsConvertor {
+	String one[] = { "zero", "one", "two", "three", "four", "five", " " + "six", "seven", "eight", "nine",
+			"ten", "eleven", "twelve", " " + "thirteen", "fourteen", "fifteen", "sixteen", "seventeen",
+			"eighteen", "nineteen"};
+	List<String> map = new ArrayList<String>(); 
+
+	public String getWordsForNumber(Integer number) {
+		Integer listIndexZero = 0;
+		Integer modulasValue = 100;
+		
+		List<String> map = numberToWord((number % modulasValue), " ");
+		
+		return map.get(listIndexZero);
+	}
+	
+	public List<String> numberToWord(Integer number, String word) {
+		map.add(one[number]);	
+		if (number > 0)
+			map.add(word);
+		
+		return map;
 	}
 
 }
--- number-to-words/src/test/java/com/prep/rockpaper/NumberToWordsTest.java
+++ number-to-words/src/test/java/com/prep/rockpaper/NumberToWordsTest.java
@@ -9,13 +9,34 @@ public class NumberToWordsTest {
 	@Test
 	public void should_convert_0_to_word() {
 		NumberToWordsConvertor convertor = new NumberToWordsConvertor();
-		assertEquals("Zero", convertor.getWordsForNumber(0));
+		assertEquals("zero", convertor.getWordsForNumber(0));
 	}
 	
 	@Test
 	public void should_convert_1_to_word() {
 		NumberToWordsConvertor convertor = new NumberToWordsConvertor();
-		assertEquals("One", convertor.getWordsForNumber(1));
+		assertEquals("one", convertor.getWordsForNumber(1));
+
+	}
+	
+	@Test
+	public void should_convert_9_to_word() {
+		NumberToWordsConvertor convertor = new NumberToWordsConvertor();
+		assertEquals("nine", convertor.getWordsForNumber(9));
+
+	}
+	
+	@Test
+	public void should_convert_10_to_word() {
+		NumberToWordsConvertor convertor = new NumberToWordsConvertor();
+		assertEquals("ten", convertor.getWordsForNumber(10));
+
+	}
+	
+	@Test
+	public void should_convert_19_to_word() {
+		NumberToWordsConvertor convertor = new NumberToWordsConvertor();
+		assertEquals("nineteen", convertor.getWordsForNumber(19));
 
 	}
 }
